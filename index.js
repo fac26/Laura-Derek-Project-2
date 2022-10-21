944545cc5cf48ac6d7e69b88e8f3004c7ffb85c7
ll835 <loktinlo@gmail.com>
2022-10-21 19:10:29 +0100

    add function to get related gif from GIPHY API
    relates #10

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const form = document.querySelector("form");
+const apiKey = "Uw5sF7tekQMGdK5DhL2yGg9JPCutP6SU";
 form.addEventListener("submit", (e) => {
     e.preventDefault(); // stop the form's default behaviour of submitting
     const input = document.querySelector("input");
     const searchTerm = input.value.toLowerCase().trim(); // save the word to a variable
     input.value = ""; // clear the search bar
     createOutput();
+    getGif(searchTerm);
 })
 
 function createOutput(){
@@ -20,9 +22,33 @@ function createOutput(){
             <h2 class="search-results-heading"></h2>
             <p class="search-results-description"></p>
         </div>
-    <img class="search-result-gif" src="" alt="">
+    <img class="search-results-gif" src="" alt="">
     </output>
     `;
     outputSection.innerHTML = outputSectionHTML; 
     document.body.appendChild(outputSection); // add new section to DOM for displaying search results
+}
+
+function getGif(word){
+    const gif = document.querySelector(".search-results-gif");
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${word}&limit=1&offset=0&rating=g&lang=en`)
+        .then(response => {
+            if (!response.ok){
+                throw new Error(response.status); // check that there isn't a error
+            }
+            return response.json()
+        })
+        .then(gifSearchResults => {
+            if (gifSearchResults.data.length === 0){
+                throw new Error("There are no related gifs"); // deal with the problem of no related gifs in the catch block
+            }
+            const gifURL = gifSearchResults.data[0].images.downsized.url;
+            const gifAltText = gifSearchResults.data[0].title;
+            gif.src = gifURL;
+            gif.alt = gifAltText;
+
+        })
+        .catch(error => {
+            // code to deal with error goes here
+        })
 }
