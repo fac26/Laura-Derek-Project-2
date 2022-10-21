6f382b84cf3835a5dc71aa45ccb68e92fe58d3a8
ll835 <loktinlo@gmail.com>
2022-10-21 21:31:07 +0100

    add error handling
    relates #10

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const form = document.querySelector("form");
-const apiKey = "Uw5sF7tekQMGdK5DhL2yGg9JPCutP6SU";
 form.addEventListener("submit", (e) => {
     e.preventDefault(); // stop the form's default behaviour of submitting
     const input = document.querySelector("input");
@@ -30,25 +29,31 @@ function createOutput(){
 }
 
 function getGif(word){
+    const apiKey = "Uw5sF7tekQMGdK5DhL2yGg9JPCutP6SU";
     const gif = document.querySelector(".search-results-gif");
     fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${word}&limit=1&offset=0&rating=g&lang=en`)
         .then(response => {
             if (!response.ok){
-                throw new Error(response.status); // check that there isn't a error
+                throw new Error(response.status); // throw error if GIPHY doesn't respond
             }
             return response.json()
         })
         .then(gifSearchResults => {
-            if (gifSearchResults.data.length === 0){
+            if (gifSearchResults.data.length === 0){ // If no gifs are returned, fetch a random gif
                 fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&limit=1&rating=g`)
-                    .then(response => response.json())
+                    .then(response => {
+                        if (!response.ok){
+                            throw new Error(response.status); // throw error if GIPHY doesn't respond
+                        }
+                       return response.json()
+                    })
                     .then(randomGif => {
                         const randomGifURL = randomGif.data.images.downsized.url;
                         const randomGifAltText = randomGif.data.title;
                         gif.src = randomGifURL;
                         gif.alt = randomGifAltText;
                     })            
-            } else {
+            } else { // If there is a related gif, use the first related gif that comes up 
             const gifURL = gifSearchResults.data[0].images.downsized.url;
             const gifAltText = gifSearchResults.data[0].title;
             gif.src = gifURL;
@@ -56,9 +61,8 @@ function getGif(word){
             }
         })
         .catch(error => {
-            if (error.message === "There are no related gifs"){
-                console.log(error);
-
-            }
+            console.log(error);
+            gif.src = "https://media4.giphy.com/media/iI4vhciiVh2b3j9sgo/giphy.gif?cid=ecf05e477g6pz5sfswgsvbkvkzrkq6lxssi2fstgewa7kvdn&rid=giphy.gif&ct=g";
+            gif.alt = "Neon planet landscape gif by dualvoidanima"; // If there's some error, use the default gif
         })
 }
